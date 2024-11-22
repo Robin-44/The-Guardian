66222749a0014ed7ea2ae22bde57d38b1eb8b564
Zoubert ichma <zoubertichma@mbp-de-ahmed.home>
2024-11-22 11:53:06 +0100

    add auth0

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import {
+  Routes,
+} from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ExternalApiComponent } from './pages/external-api/external-api.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { authGuardFn } from '@auth0/auth0-angular';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+
+export const routes: Routes = [
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [authGuardFn],
+  },
+  {
+    path: 'external-api',
+    component: ExternalApiComponent,
+    canActivate: [authGuardFn],
+  },
+  {
+    path: 'error',
+    component: ErrorComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuardFn] },
+];
