9e7ef331a649bf509a80878271d52d5dbc7aace6
User <robin.mahe@epitech.digital>
2024-12-04 22:16:44 +0100

    style modal modification de mes informations

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { AsyncPipe, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { HighlightModule } from 'ngx-highlightjs';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-profile',
@@ -11,6 +12,57 @@ import { HighlightModule } from 'ngx-highlightjs';
   imports: [HighlightModule, AsyncPipe, NgIf],
 })
 export class ProfileComponent implements OnInit {
+  formData = {
+    userName: 'Jean',
+    userAge: 25,
+    userGender: 'Homme',
+    userWeight: 75,
+    userSize: 180,
+    userBloodType: 'O+',
+    userComments: 'Aucun'
+  };
+
+  // Ouvrir la modale de modification
+  openEditModal() {
+    const editModal = new bootstrap.Modal(document.getElementById('editModal'));
+    editModal.show();
+  }
+
+  // Sauvegarder les changements effectués
+  saveChanges() {
+    // Effectuer les sauvegardes ou validations nécessaires ici
+    // Fermer la modale après la sauvegarde
+    const editModal = new bootstrap.Modal(document.getElementById('editModal'));
+    editModal.hide(); // Utilisation de la méthode hide() pour fermer la modale
+  }
+
+  // Ouvrir la modale de suppression
+  openDeleteModal() {
+    const editModal = new bootstrap.Modal(document.getElementById('deleteModal'));
+    editModal.show();
+  }
+
+  // Supprimer les données utilisateur
+  deleteData() {
+    // Effectuer les sauvegardes ou validations nécessaires ici
+    // Fermer la modale après la sauvegarde
+    const editModal = new bootstrap.Modal(document.getElementById('deleteModal'));
+    editModal.hide(); // Utilisation de la méthode hide() pour fermer la modale
+  }
+
+  // // Supprimer les données utilisateur
+  // deleteData() {
+  //   this.formData = {
+  //     userName: '',
+  //     userAge: null,
+  //     userGender: '',
+  //     userWeight: null,
+  //     userSize: null,
+  //     userBloodType: '',
+  //     userComments: ''
+  //   };
+  //   $('#deleteModal').modal('hide'); // Fermer la modale
+  // }
   profileJson: string = null;
 
   constructor(public auth: AuthService) {}
